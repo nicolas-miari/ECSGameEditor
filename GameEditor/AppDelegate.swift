fdbe5365cdb7d04793084615c8875bc90d735f0e
Nicolás Miari <nicolasmiari@gmail.com>
2022-08-07 11:24:05 +0900

    Initial Commit

---
+++ GameEditor/AppDelegate.swift
@@ -0,0 +1,29 @@
+//
+//  AppDelegate.swift
+//  GameEditor
+//
+//  Created by Nicolás Miari on 2022/08/07.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+  func applicationDidFinishLaunching(_ aNotification: Notification) {
+    // Insert code here to initialize your application
+  }
+
+  func applicationWillTerminate(_ aNotification: Notification) {
+    // Insert code here to tear down your application
+  }
+
+  func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+    return true
+  }
+
+
+}
+
---
+++ GameEditor/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  GameEditor
+//
+//  Created by Nicolás Miari on 2022/08/07.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+
+  override var representedObject: Any? {
+    didSet {
+    // Update the view, if already loaded.
+    }
+  }
+
+
+}
+
